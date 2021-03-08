5fabc93adbbe7c2f85a78f8549118822309e497b
SWFTR <mirko.scheschonka@gmail.com>
2021-03-08 16:31:59 +0100

    added sidebar, just missing hover effects

--- youtube-ui-clone-react/src/App.js
+++ youtube-ui-clone-react/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
    <>
     <Header/>
+    <Sidebar/>
    </>
   );
 }
