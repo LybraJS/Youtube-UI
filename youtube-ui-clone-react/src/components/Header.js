5ccea4370169fdb3ea2746cb2e46aeba0ff486bb
SWFTR <mirko.scheschonka@gmail.com>
2021-03-08 12:55:40 +0100

    resolved bug of npm not finding file

--- youtube-ui-clone-react/src/components/Header.js
+++ youtube-ui-clone-react/src/components/Header.js
@@ -1,1 +1,11 @@
-console.log('Hello World')
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+export default Header
